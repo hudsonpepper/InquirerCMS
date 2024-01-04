71383ebd0e9ba1791667a42c5655ada3a68b9b87
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-01-04 18:43:15 -0500

    Getting close on add employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,15 +12,15 @@ VALUES  ("Head of Sales", 120000, 1),
         ("Customer Service Worker", 40000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Bruno", "Meyers", 1, 1),
+VALUES  ("Bruno", "Meyers", 1, NULL),
         ("Aria", "Schroeder", 2, 1),
         ("Kamora", "Phillips", 2, 1),
-        ("Brody", "Mosley", 3, 4),
+        ("Brody", "Mosley", 3, NULL),
         ("Marcel", "Perry", 4, 4),
         ("Ari", "Gilbert", 4, 4),
         ("Abagail", "Simmons", 4, 4),
-        ("Irvin", "Valencia", 5, 8),
+        ("Irvin", "Valencia", 5, NULL),
         ("Kane", "Chapman", 6, 8),
         ("Keshawn", "Brown", 6, 8),
         ("Angelo", "Nolan", 6, 8),
-        ("Rene", "Mcguire", 6, 8);
+        ("Rene", "Mcguire", 6, 8)
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
+//const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const { Console } = require('console');
+//const PORT = process.env.PORT || 3001;
+//const app = express();
 
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
 
 
 const db = mysql.createConnection(
@@ -14,28 +15,33 @@ const db = mysql.createConnection(
     host: 'localhost',
     user: 'root',
     password: 'rootroot',
-    database: 'books_db'
+    database: 'departments_db'
   },
   console.log("Connected to the departments_db database.")
 );
 
-function init() {
+async function init() {
   inquirer
     .prompt([
       {
         type: 'list',
         name: 'actionSelection',
         message: 'What would you like to do?',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Exit']
       }
-    ]).then((response) => {
+    ]).then(async (response) => {
       console.log(response);
-      switch(response.actionSelection){
+      switch (response.actionSelection) {
         case 'View All Employees':
           console.log('You selected "View All Employees"');
+          viewAllEmployees().then((data) => {
+            console.table(data[0]);
+            init();
+          })
           break;
         case 'Add Employee':
           console.log('You Selected "Add Employee"');
+          addEmployee();
           break;
         case 'Update Employee Role':
           console.log('You selected "Update Employee Role"');
@@ -52,9 +58,60 @@ function init() {
         case 'Add Department':
           console.log('You selected "Add Department"');
           break;
+        case 'Exit':
+          console.log("Bye!");
+          process.exit();
         default:
           console.log('Incorrect Selection');
       }
-    })
+    })//.then(init)
 }
 init();
+
+function viewAllEmployees() {
+  let results = db.promise().query(`SELECT A.id, A.first_name, A.last_name, B.first_name AS Manager_FName, B.last_name AS Manager_LName FROM employee A, employee B WHERE A.manager_id = B.id`)
+  return results;
+};
+function addEmployee() {
+  viewManagerNamesPromise().then((data) => {
+    console.log(data)
+    let nameArr = data[0].map((x) => `${x.first_name} ${x.last_name}`);
+    console.log(nameArr);
+    console.log(concatNames(data));
+    //init();
+    return (nameArr)
+  }).then((rolesArr, potManagerArr) => {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'first_name',
+          message: "Enter employee's first name: ",
+          validate(input) {
+            return input.length > 0;
+          }
+        },
+        {
+          type: 'input',
+          name: 'last_name',
+          message: "Enter employee's last name: ",
+          validate(input) {
+            return input.length > 0;
+          }
+        }
+      ])
+  })
+}
+function viewEmployeeNamesPromise() {
+  let results = db.promise().query(`SELECT employee.first_name, employee.last_name FROM employee`)
+  return results;
+}
+function viewManagerNamesPromise() {
+  let results = db.promise().query(`SELECT employee.first_name, employee.last_name FROM employee WHERE employee.id IN (SELECT employee.manager_id FROM employee)`);
+  return results;
+}
+
+function concatNames(data) {
+  let nameArr = data[0].map((x) => `${x.first_name} ${x.last_name}`);
+  return nameArr;
+}
