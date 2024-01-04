76d1f57ac5b1e518bde3189a91be1e82ed861607
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-01-04 09:20:54 -0500

    added package.json

--- db/schema.sql
+++ db/schema.sql
@@ -6,30 +6,27 @@ CREATE DATABASE departments_db;
 USE departments_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   -- Name is a Keyword in sql, so I changed it to dep_name
-  dep_name VARCHAR(30),
-  PRIMARY KEY (id)
+  dep_name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
-  PRIMARY KEY (id),
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT, 
-  PRIMARY KEY (id),
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
 )
